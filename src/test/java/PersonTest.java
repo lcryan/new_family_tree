77b55712e70fbd8dd71283872e558804fc058dbd
LuciaRyan <lcryan06@gmail.com>
2023-06-24 18:51:39 +0200

    test faulty - debugged

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,9 +1,18 @@
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-class PersonTest {
+public class PersonTest {
     @Test
     void getName() {
 
+        //arrange //
+        Person Luisa = new Person("Luisa", "Madrigal", 18, "female");
+
+        //act//
+        String name = Luisa.getName();
+
+        //Assert//
+        assertEquals("Luisa", name);
     }
 
 }
