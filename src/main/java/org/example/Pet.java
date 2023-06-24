84010cf19172f3effeae5a247de55a7e4eb27637
LuciaRyan <lcryan06@gmail.com>
2023-06-24 16:36:56 +0200

    removed final of owner / this might be able to change or there might be more than one owner ?

--- src/main/java/org/example/Pet.java
+++ src/main/java/org/example/Pet.java
@@ -1,8 +1,8 @@
 package org.example;
 
-public class Pet {
 
-    final private String name;
+public class Pet {
+    private String name;
     private int age;
     private String species;
     private String owner;
